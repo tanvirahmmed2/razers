0f0c0df4d3956d16e337a65296e6ca65a898d062
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-11 23:43:42 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './layout/Navbar'
+import Footer from './layout/Footer'
+import Cart from './pages/Cart'
+import About from './pages/About'
+import Home from './layout/Home'
 
 const App = () => {
   return (
     <>
-      
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </div>
+
+
+      <Footer />
     </>
   )
 }
