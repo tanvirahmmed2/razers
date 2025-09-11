81b37b6d2ee58b1796ef0373080b63588eefb155
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-11 23:59:41 +0600

    update

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <section>
-        <footer>
-            
+    <section className='m-2'>
+        <footer className='w-full flex flex-col items-center justify-center p-4 bg-gray-200 rounded-lg'>
+            <div className='w-full flex-col items-start justify-center'></div>
+            <div className='w-full flex-col items-start justify-center'></div>
+            <p>Allrights are reserved by Razers</p>
+
         </footer>
     </section>
   )
