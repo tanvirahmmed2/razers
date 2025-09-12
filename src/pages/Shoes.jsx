0da2b37db703f50b89c6e903f4affe1dec139179
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-12 12:30:22 +0600

    update

--- src/pages/Shoes.jsx
+++ src/pages/Shoes.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Shoes = (props) => {
     const {name, id, new_price, old_price, image}= props
   return (
-    <div className='w-[250px] h-[300px] rounded-lg relative bg-gray-500 group shadow-sm overflow-hidden shadow-blue-400'>
+    <div className='md:w-[250px] md:h-[300px] w-[150px] h-[200px] rounded-lg relative bg-gray-500 group shadow-sm overflow-hidden shadow-blue-400'>
         <img src={image} alt="" className='w-[250px] h-[300px] object-cover group-hover:scale-110 group-hover:rotate-2 transition duration-500'/>
         <div className=' w-full h-auto hidden transition duration-500 flex-col items-center justify-center bg-white absolute bottom-0 p-2 group-hover:flex'>
             <Link to={`/shoe/${id}`} className='font-semibold'>{name}</Link>
