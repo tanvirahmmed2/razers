275fa29ce1fcb49f0f36fc954c68472ee849f292
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-14 17:03:33 +0600

    update

--- src/user/Register.jsx
+++ src/user/Register.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='w-full h-auto my-20 flex items-center justify-center'>
+        <div className='w-auto h-auto p-4 flex flex-col lg:flex-row items-center justify-center gap-6 bg-sky-100 rounded-lg'>
+            <div className='w-auto flex flex-col items-center justify-center gap-2'>
+                <h1 className='text-2xl '>Welcome  to <span className='text-3xl font-bold'>Razers</span></h1>
+                <p>register and enjoy our premium services</p>
+                <Link to='/login' className='mt-6 text-green-500 italic'>already registered?</Link>
+            </div>
+            <form action="" className='flex flex-col items-center justify-center gap-4 lg:border-l-2 pl-4 border-white'>
+               
+                <div className='w-auto flex flex-col items-start justify-center gap-2'>
+                    <label htmlFor="name">name</label>
+                    <input type="text" name='name' id='name' className='p-1 px-2 border-2 outline-none rounded-lg'/>
+                </div>
+                <div className='w-auto flex flex-col items-start justify-center gap-2'>
+                    <label htmlFor="email">email</label>
+                    <input type="email" name='email' id='email' className='p-1 px-2 border-2 outline-none rounded-lg'/>
+                </div>
+                <div className='w-auto flex flex-col items-start justify-center gap-2'>
+                    <label htmlFor="password">password</label>
+                    <input type="password" name='password' id='password' className='p-1 px-2 border-2 outline-none rounded-lg'/>
+                </div>
+                <button type='submit' className='p-1 px-2 bg-white text-sky-600 font-semibold rounded-lg'>Continue</button>
+
+            </form>
+
+        </div>
+    </section>
   )
 }
 
